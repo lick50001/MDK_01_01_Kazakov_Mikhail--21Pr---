23e7280591a2e4057d138d3c80aa03f672b26c3e
qwelord <secondofall22@rambler.ru>
2025-11-24 19:28:14 +0500

    Реализованные интерфейсы. Казаков

---
+++ Documents_Kazakov/Interfaces/IDocument.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Documents_Kazakov.Model;
+
+namespace Documents_Kazakov.Interfaces
+{
+    public interface IDocument
+    {
+        void Save(bool Update = false);
+        List<Documents.Classes.DocumentContext> AllDocuments();
+        void Delete();
+    }
+}
